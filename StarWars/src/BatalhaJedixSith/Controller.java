4e85a05a75a767e3bf28bbeb9555b416961ea064
matheus <matheusguilhermeg@gmail.com>
2018-05-23 23:49:59 -0300

    Algumas modificacoes iniciais para modelar a batalha Jedi

---
+++ StarWars/src/BatalhaJedixSith/Controller.java
@@ -0,0 +1,21 @@
+package BatalhaJedixSith;
+
+public class Controller {
+	private EventSet es = new EventSet();
+	
+	public void addEvent(Event c) { 
+		es.add(c);
+	}
+	
+	public void run() {
+		Event e;
+		while((e = es.getNext()) != null) {
+			if(e.ready()) {
+				e.action();
+				System.out.println(e.description());
+				es.removeCurrent();
+			}
+		}
+	}
+	
+}
